0760df2170989e69ea804284cfb28a5da3fb91fb
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-03-27 13:50:33 +0330

    Create base layout and stylesheet file and html bod for Home page and About page

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,31 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Ali Darvishi',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Mar 27, 2023'
+    },
+    {
+        'author': 'Sahar Hashemi',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Mar 28, 2023'
+    }
+]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == "__main__":
