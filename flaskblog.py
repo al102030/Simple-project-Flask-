4b232a43d164ea1dcbd2e8055eea9456fe94c618
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-03-28 22:16:13 +0330

    Add registeration and login page and create flask forms clssses.

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, flash, redirect, url_for
+from config import token
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = token
 
 posts = [
     {
@@ -26,6 +30,27 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created For { form.username.data }!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been loged in!", "success")
+            return redirect(url_for('home'))
+        else:
+            flash("Login Failed. Please check username and password.", "danger")
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == "__main__":
