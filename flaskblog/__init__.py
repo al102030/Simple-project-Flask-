7647863b074cf4f63f82ee989cbba1c208f8074e
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-04-02 15:18:52 +0330

    Add routes and forms for sending email with instruction to change password as url.(add 2 comment for saving email and password as varable in system)

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -17,7 +17,9 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
+# Your email address that saved in environement varable (bash_profile)
 app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USERNAME')
+# Your email password that saved in environement varable (bash_profile)
 app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
 mail = Mail(app)
 
