8e6bb13d99fa43e046e51ec9b2173c2e767b3c7d
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-04-03 14:35:46 +0330

    Add error handler and some beautful error pages to application.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -27,8 +27,10 @@ def create_app(config_class=Config):
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
